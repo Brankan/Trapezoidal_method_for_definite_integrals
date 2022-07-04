f1090a2c4564c389c7a6ac4e2903fb2a247085f5
Brankan <60178858+Brankan@users.noreply.github.com>
2022-07-04 19:08:37 +0200

    Update integral.py

--- integral.py
+++ integral.py
@@ -16,29 +16,29 @@ okand = []
 h = float(b - a) / n
 s = 0.0
 x = a
-s += eval(f)/2.0
-for i in range(1, n):
-    x = a + i*h
+s += eval(f) / 2.0
+for i in range(1 , n):
+    x = a + i * h
     s += eval(f)
 x = b
 s += eval(f)/2.0
 
-print('trapezoidal method: '+str(s*h))
+print('trapezoidal method: ' + str(s*h))
 
 
 
 x, y, z, t = symbols('x y z t')
-ff=integrate(eval(f),x,)
-print('Integral result: '+str(ff))
+ff=integrate(eval(f) , x ,)
+print('Integral result: '+ str(ff))
 pprint(ff)
 x = a
 aend = eval(str(ff))
 x = b
 bend = eval(str(ff))
 res = bend-aend
-print('Integral result:'+str(res))
+print('Integral result:' + str(res))
 
-for xd in range(-200,200):
+for xd in range(-200 , 200):
     firstarr.append(xd)
     x = xd
     originf.append(eval(str(ff)))
@@ -49,16 +49,16 @@ for xd in range(-200,200):
     s = 0.0
     x = a
     s += eval(f) / 2.0
-    for i in range(1, n):
+    for i in range(1 , n):
         x = a + i * h
         s += eval(f)
     x = b
     s += eval(f) / 2.0
-    secondarr.append(s*h)
+    secondarr.append(s * h)
 
 
-plt.plot(firstarr, originf, label='1')
-plt.plot(firstarr, secondarr, label='2)')
-plt.plot(firstarr, okand, label='3)')
+plt.plot(firstarr, originf, label = '1')
+plt.plot(firstarr, secondarr, label = '2)')
+plt.plot(firstarr, okand, label = '3)')
 plt.axis([-100, 100, -100, 100])
 plt.show()
