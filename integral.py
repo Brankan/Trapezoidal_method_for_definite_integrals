fbc716b4e2d01b38eaa6401ff59c3b18b87a364a
Brankan <60178858+Brankan@users.noreply.github.com>
2022-07-04 18:53:18 +0200

    Update integral.py

--- integral.py
+++ integral.py
@@ -2,26 +2,25 @@ from sympy import *
 import math
 from sympy.abc import a, x, y
 from sympy import init_printing
-
 import matplotlib.pyplot as plt
 
-f =input("Type function:")
-a =int(input('a:'))
-b =int(input('b:'))
-n =int(input('m:'))
-firstarr=[]
-secondarr=[]
-originf=[]
-okand=[]
+f = input("Type function:")
+a = int(input('a:'))
+b = int(input('b:'))
+n = int(input('m:'))
+firstarr = []
+secondarr = []
+originf = []
+okand = []
 
 h = float(b - a) / n
 s = 0.0
-x=a
+x = a
 s += eval(f)/2.0
 for i in range(1, n):
-    x=a + i*h
+    x = a + i*h
     s += eval(f)
-x=b
+x = b
 s += eval(f)/2.0
 
 print('trapezoidal method: '+str(s*h))
@@ -32,20 +31,20 @@ x, y, z, t = symbols('x y z t')
 ff=integrate(eval(f),x,)
 print('Integral result: '+str(ff))
 pprint(ff)
-x=a
-aend=eval(str(ff))
-x=b
-bend=eval(str(ff))
-res=bend-aend
+x = a
+aend = eval(str(ff))
+x = b
+bend = eval(str(ff))
+res = bend-aend
 print('Integral result:'+str(res))
 
 for xd in range(-200,200):
     firstarr.append(xd)
-    x=xd
+    x = xd
     originf.append(eval(str(ff)))
     okand.append(eval(f))
-    b=x
-    a=x-1
+    b = x
+    a = x - 1
     h = float(b - a) / n
     s = 0.0
     x = a
@@ -58,8 +57,8 @@ for xd in range(-200,200):
     secondarr.append(s*h)
 
 
-plt.plot(firstarr, originf, label='x+1')
-plt.plot(firstarr, secondarr, label='Furje(x+1)')
-plt.plot(firstarr, okand, label='2)')
+plt.plot(firstarr, originf, label='1')
+plt.plot(firstarr, secondarr, label='2)')
+plt.plot(firstarr, okand, label='3)')
 plt.axis([-100, 100, -100, 100])
 plt.show()
